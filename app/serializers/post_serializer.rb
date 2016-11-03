4fd86699ceecf9fc2c90640458d8cba8b42330c4
fidelscodes <fidelscodes@gmail.com>
2016-11-03 16:31:50 -0400

    Done

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,6 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description
+  # here we have to explicitly let the post know what serializer to user for its author
+  # rather than relying on convention
+  has_one :author, serializer: PostAuthorSerializer
+end
